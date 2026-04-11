99f24b60d71de51842e9f9c2df14e786a4e9ae24
Sakar Upadhyaya Khatiwada <urssakar@gmail.coM>
2026-04-11 18:42:10 +0545

    The sitemap is change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,23 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+	site: 'https://sakaru.com.np',
+	integrations: [
+		react(), 
+		sitemap({
+			serialize(item) {
+				if (item.url === 'https://sakaru.com.np/' || item.url.includes('/services')) {
+					item.priority = 1.0;
+					item.changefreq = 'daily';
+				} else if (item.url.includes('/blog') || item.url.includes('/talks') || item.url.includes('/portfolio')) {
+					item.priority = 0.8;
+					item.changefreq = 'weekly';
+				}
+				return item;
+			},
+		})
+	],
 });
